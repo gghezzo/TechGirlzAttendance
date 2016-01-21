dfc47cebf6f5af42e328302193579856c0d52b4b
gghezzo <ginnyghezzo@gmail.com>
2016-01-20 20:55:46 -0500

    Initial report

--- Parse2015Attendance.py
+++ Parse2015Attendance.py
@@ -11,11 +11,47 @@
 import csv
 import datetime
 
-file = input("What is the file to parse")
+file = input("What is the file to parse ")
 print(file)
 try:
 	f = open(file)
 	eventCSV = csv.DictReader(f)
 except (IOError, ValueError): 
 	print("An I/O Error or Value occurred. No file. We are DONE HERE!!! ")
-	exit()
+	exit()		#Not sure if this is needed ... TODO think about this 
+
+attendees = {}
+oneShows = set()
+shows = set()
+twoShows = set()
+for row in eventCSV: 
+	# print(row['Email'])
+	# print(row['Attendee Status'])
+	email = row['Email']
+	status = row['Attendee Status']
+	eventName = row['Event Name']
+	if  not (email in attendees):
+		attendees[email] = [],[]
+	if status == 'Checked In': 
+		attendees[email][0].append(eventName)
+	else: 	# Either Attending or Not Attending 
+		attendees[email][1].append(eventName)
+
+
+# print(attendees)
+for i in attendees: 
+	print("Email ", i)
+	print("   Attended: ",len(attendees[i][0]))
+	print("   Missed: ",len(attendees[i][1]))
+	if len(attendees[i][1]) >= 2: 
+		twoShows.add(i)
+	elif len(attendees[i][1]) == 1: 
+		oneShows.add(i)
+	else: 
+		shows.add(i)
+print("Two or More Noshows) " , len(twoShows))
+print(twoShows)
+print("One No Shows " , len(oneShows))
+print("Shows " , len(shows))
+
+
